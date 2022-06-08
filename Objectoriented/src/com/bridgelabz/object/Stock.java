ed0db127afba52add0fc9ae46af6f94dd9af6fed
sireeshakenguva <sireeshakenguva@gmail.com>
2022-06-08 10:36:24 +0530

    StockAccountManagement program

---
+++ Objectoriented/src/com/bridgelabz/object/Stock.java
@@ -0,0 +1,45 @@
+package com.bridgelabz.object;
+
+public class Stock {
+    String stockName;
+    int numberOfShares;
+    int sharePrice;
+
+
+   public Stock(String stockName, int numberOfShares, int sharePrice) {
+
+       this.stockName = stockName;
+       this.numberOfShares = numberOfShares;
+       this.sharePrice = sharePrice;
+   }
+
+   public String getStockName() {
+       return stockName;
+   }
+
+   public void setStockName(String stockName) {
+       this.stockName = stockName;
+   }
+
+   public int getNumberOfShares() {
+       return numberOfShares;
+   }
+
+   public void setNumberOfShares(int numberOfShares) {
+       this.numberOfShares = numberOfShares;
+   }
+
+   public int getSharePrice() {
+       return sharePrice;
+   }
+
+   public void setSharePrice(int sharePrice) {
+       this.sharePrice = sharePrice;
+   }
+
+   @Override
+   public String toString() {
+       return "Stock [stockName=" + stockName + ", numberOfShares=" + numberOfShares + ", sharePrice=" + sharePrice
+               + "]";
+   }
+}
